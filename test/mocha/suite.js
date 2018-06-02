def52b1c87754e89ee4649e249ddc6cf7556c3b7
Eliot Baker <eliotbaker@gmail.com>
2018-06-01 18:32:56 -0700

    update tests to es2015/2017. add 2 more board count tests

--- test/mocha/suite.js
+++ test/mocha/suite.js
@@ -14,34 +14,82 @@ if (typeof require == 'function') {
 
 var sweeper;
 
-describe('sweeper', function() {
+describe('sweeper', () => {
+
   before(function(){
     sweeper = new Sweeper();
   });
-  it('smoke test should pass', function () {
+
+  it('smoke test should pass', () => {
     assert.equal(sweeper.smokeTest(), 'smoke test', 'failure message');
   });
-  it('reset board', function() {
+
+  it('should reset board', () => {
     sweeper.resetBoard(2, 2);
     var empty2x2 = [[undefined, undefined], [undefined, undefined]];
+
     assert.deepEqual(sweeper.board, empty2x2, '2x2 board not reset');
     sweeper.resetBoard();
     assert.deepEqual(sweeper.board, empty2x2, '2x2 default board not reset');
   });
-  it('generates random coords', function(){
-    var generated = sweeper.genRandSquares([0,0], 2);
-    var max = sweeper.height * sweeper.width;
+
+  it('should generates random bounded coords', () => {
+    const generated = sweeper.genRandSquares([0,0], 2);
+    const max = sweeper.height * sweeper.width;
+ 
     generated.map(function(square){
       assert.equal(square > -1, true, 'square less than 0');
       assert.equal(square != 0, true, 'excluded square selected');
       assert.equal(square < max, true, 'square greater than max');
     });
   });
-  it('add bombs with hints', function(){
-    var bombs = [1];
-    var checkBoard = [[1, '*'], [1, 1]];
+
+  it('should add one bomb with hints to 2x2', () => {
+    const bombs = [1];
+    const check2x2Board = [
+      [ 1, '*' ],
+      [ 1,  1  ]
+    ];
+
+    assert.equal(bombs.length, 1);
+
     sweeper.resetBoard(2, 2);
     sweeper.addBombsWithHints(bombs);
-    assert.deepEqual(sweeper._board, checkBoard, '2x2 one bomb added');
+
+    assert.deepEqual(sweeper._board, check2x2Board, '2x2 one bomb');
+  });
+
+  it('should add two bombs with hints to 3x3', () => {
+    const bombs = [2, 8];
+    const check3x3Board = [
+      [0, 1, '*'],
+      [0, 2,  2 ],
+      [0, 1, '*']
+    ];
+
+    assert.equal(bombs.length, 2);
+
+    sweeper.resetBoard(3, 3);
+    sweeper.addBombsWithHints(bombs);
+
+    assert.deepEqual(sweeper._board, check3x3Board, '3x3 two bombs');
+  });
+
+  it('should add eleven bombs with hints to 5x5', () => {
+    const bombs = [1, 2, 3, 6, 8, 9, 11, 12, 13, 18, 19];
+    const check3x3Board = [
+      [2, '*', '*', '*',  3  ],
+      [3, '*',  8 , '*', '*' ],
+      [2, '*', '*', '*',  5  ],
+      [1,  2 ,  4 , '*', '*' ],
+      [0,  0 ,  1,   2 ,  2  ]
+    ];
+
+    assert.equal(bombs.length, 11);
+
+    sweeper.resetBoard(5, 5);
+    sweeper.addBombsWithHints(bombs);
+
+    assert.deepEqual(sweeper._board, check3x3Board, '5x5 eleven bombs');
   });
 });
